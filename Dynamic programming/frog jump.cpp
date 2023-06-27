bb694f80d4bc48dce32b9e95fcf454e9d3d0ffcb
Infinity365 <93903210+Infinity365@users.noreply.github.com>
2023-06-27 17:41:45 +0530

    Create frog jump.cpp

---
+++ Dynamic programming/frog jump.cpp
@@ -0,0 +1,24 @@
+https://www.codingninjas.com/studio/problems/frog-jump_3621012?leftPanelTab=1
+
+#include <bits/stdc++.h> 
+int fun(int ind,vector<int>&heights, vector<int>&dp){
+    if(ind==0)
+    return 0;
+    if(dp[ind]!=-1)
+    return dp[ind];
+    int right=INT_MAX;
+    int left=fun(ind-1,heights,dp)+abs(heights[ind]-heights[ind-1]);
+    if(ind>1){
+        right=fun(ind-2,heights,dp)+abs(heights[ind]-heights[ind-2]);
+    
+    }
+    dp[ind]=min(left,right);
+    return dp[ind];
+}
+int frogJump(int n, vector<int> &heights)
+{
+    // Write your code here.
+    
+    vector<int> dp(n+1,-1);
+    return fun(n-1,heights,dp);
+}
