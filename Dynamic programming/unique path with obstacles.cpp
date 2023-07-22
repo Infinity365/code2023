db07c783c6b294a94c622144811bc5d7b19d1838
Infinity365 <93903210+Infinity365@users.noreply.github.com>
2023-07-22 11:02:33 +0530

    Update and rename unique path 2.cpp to unique path with obstacles.cpp

--- Dynamic programming/unique path with obstacles.cpp
+++ Dynamic programming/unique path with obstacles.cpp
@@ -1,3 +1,43 @@
+
+//space optimization
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int n,m;
+        n=grid.size();
+        m=grid[0].size();
+         vector<int> prev(m,0);
+        if(grid[0][0]==1)
+        return 0;
+        for(int i=0;i<n;i++){
+            vector<int> cur(m,0);
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0)
+                cur[j]=1;
+                
+                else if(grid[i][j]==1)
+                cur[j]=0;
+                
+                else{
+                    int right,down;
+                    right=down=0;
+                    if(i>0){
+                        right=prev[j];
+                    }
+                    if(j>0){
+                        down=cur[j-1];
+                    }
+                    cur[j]=right+down;
+                }
+
+                
+            }
+            prev=cur;
+        }
+        
+        return prev[m-1];
+    }
+};
 // tabulation
 int mod=(int)(1e9+7);
 int mazeObstacles(int n, int m, vector< vector< int> > &mat) {
