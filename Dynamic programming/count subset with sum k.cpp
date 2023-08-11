bc01a73ce5f686250419e3f755949a302cd31121
Infinity365 <93903210+Infinity365@users.noreply.github.com>
2023-08-11 22:53:03 +0530

    Update count subset with sum k.cpp

--- Dynamic programming/count subset with sum k.cpp
+++ Dynamic programming/count subset with sum k.cpp
@@ -1,26 +1,30 @@
-int findWays(vector<int>& arr, int k)
-
+int findWays(vector<int>& num, int k)
 {
-	int n=arr.size();
-	vector<vector<int>>dp(n,vector<int>(k+1,0));
-	for(int i=0;i<n;i++){
-		dp[i][0]=1;
-	}
-	if(arr[0]<=k)
-	dp[0][arr[0]]=1;
+     int n = num.size();
+int mod=1e9+7;
+    vector<vector<int>> dp(n,vector<int>(k+1,0));
+    
+    for(int i=0; i<n; i++){
+        dp[i][0] = 1;
+    }
+    
+    if(num[0]<=k)
+        dp[0][num[0]] = 1;
+    
+    for(int ind = 1; ind<n; ind++){
+        for(int target= 1; target<=k; target++){
+            
+            int notTaken = dp[ind-1][target];
+    
+            int taken = 0;
+                if(num[ind]<=target)
+                    taken = dp[ind-1][target-num[ind]];
+        
+            dp[ind][target]= (notTaken + taken)%mod;
+        }
+    }
+    
+    return dp[n-1][k];
+    
 
-	for(int i=1;i<n;i++){
-		for(int j=1;j<=k;j++){
-		int take,nottake;
-		take=nottake=0;
-		if(arr[i]<=j){
-			take=dp[i-1][j-arr[i]];
-		}
-		nottake=dp[i-1][j];
-		dp[i][j]=take+nottake;
-		}
-	}
-	int ans=dp[n-1][k];
-
-	return (ans%1000000007);
 }
